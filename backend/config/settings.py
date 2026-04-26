38743f0070834a37d7f50024b96a41c656f74646
IliasBekazarov <iliasbeknazarov4@gmail.com>
2026-04-26 21:11:41 +0800

    fix: add STATIC_ROOT for collectstatic

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -70,7 +70,8 @@ TIME_ZONE = 'Asia/Bishkek'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
